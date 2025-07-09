a65233258727ea05a7f9a00e70e6a4c5ee8ab1cb
Lgarbutt <landen12garbutt@gmail.com>
2025-07-09 06:24:02 -0700

    Add files via upload
    add retail sentiment

--- edgefinder_dashboard.py
+++ edgefinder_dashboard.py
@@ -285,16 +285,14 @@ st.components.v1.html(f"""
 # --- Myfxbook Sentiment Widget ---
 st.markdown("### 🧠 Myfxbook Trader Sentiment")
 st.components.v1.html("""
-<!-- myfxbook.com outlook widget - Start -->
 <div>
-  <script class="powered" type="text/javascript" 
-    src="https://widgets.myfxbook.com/scripts/fxOutlook.js?type=8&symbols=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,31,34,36,37,38,40,41,42,43,45,46,47,48,49,50,51,103,107,131,137,1233,1234,1236,1245,1246,1247,1815,1863,1893,2012,2076,2090,2114,2115,2119,2521,2603,2872,3003,3443,5079,5435,5779"></script>
+  <script type="text/javascript" src="https://widgets.myfxbook.com/scripts/fxOutlook.js?type=8"></script>
   <div style="font-size: 10px">
-    <a href="https://www.myfxbook.com" title="" class="myfxbookLink" target="_self" rel="noopener">
+    <a href="https://www.myfxbook.com" class="myfxbookLink" target="_blank" rel="noopener">
       Powered by Myfxbook.com
     </a>
   </div>
+  <script type="text/javascript">showOutlookWidget();</script>
 </div>
-<script type="text/javascript">showOutlookWidget();</script>
-<!-- myfxbook.com outlook widget - End -->
 """, height=400)
+
